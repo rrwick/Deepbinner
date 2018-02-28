094507e33afd17d51acd35c95560545d8d14a62d
Ryan Wick <rrwick@gmail.com>
2018-02-28 23:55:40 +1100

    Memory usage fix: don't keep all signals in memory

--- cnn_demultiplexer/load_fast5s.py
+++ cnn_demultiplexer/load_fast5s.py
@@ -1,70 +1,7 @@
 
 
-import sys
 import os
 import h5py
-
-from .trim_signal import too_much_open_pore
-
-
-def get_signal_from_fast5s(fast5_dir, signal_size, max_start_end_margin, min_signal_length):
-    fast5_files = find_all_fast5s(fast5_dir)
-    print('Loading signal from ' + str(len(fast5_files)) + ' fast5 files',
-          end='', file=sys.stderr)
-    signals = {}
-    i = 0
-
-    short_count = 0
-    bad_signal_count = 0
-
-    for fast5_file in fast5_files:
-        i += 1
-        if i % 100 == 0:
-            print('.', end='', file=sys.stderr, flush=True)
-
-        read_id, signal = get_read_id_and_signal(fast5_file)
-        if read_id is None:
-            continue
-
-        if len(signal) < min_signal_length:
-            short_count += 1
-            continue
-
-        bad_signal = False
-
-        # The middle signal is simply the centre-most signal in the read.
-        middle_pos = len(signal) // 2
-        middle_1 = middle_pos - (signal_size // 2)
-        middle_2 = middle_pos + (signal_size // 2)
-        middle_signal = signal[middle_1:middle_2]
-
-        start_margin = signal_size * 2
-        while too_much_open_pore(signal[:start_margin]):
-            start_margin += signal_size
-            if start_margin > max_start_end_margin:
-                bad_signal = True
-                break
-
-        end_margin = signal_size * 2
-        while too_much_open_pore(signal[-end_margin:]):
-            end_margin += signal_size
-            if end_margin > max_start_end_margin:
-                bad_signal = True
-                break
-
-        if bad_signal:
-            bad_signal_count += 1
-            continue
-
-        start_signal = signal[:start_margin]
-        end_signal = signal[-end_margin:]
-
-        signals[read_id] = (start_signal, middle_signal, end_signal)
-
-    print(' done', file=sys.stderr)
-    print('skipped ' + str(short_count) + ' reads for being too short\n', file=sys.stderr)
-    print('skipped ' + str(bad_signal_count) + ' reads for bad signal stdev\n', file=sys.stderr)
-    return signals
 
 
 def get_read_id_and_signal(fast5_file):
--- cnn_demultiplexer/porechop.py
+++ cnn_demultiplexer/porechop.py
@@ -1,17 +1,84 @@
 
 
 import re
-from .load_fast5s import get_signal_from_fast5s
+import sys
+from .load_fast5s import get_read_id_and_signal, find_all_fast5s
+from .trim_signal import too_much_open_pore
 
 
 def training_data_from_porechop(args):
-    signals = get_signal_from_fast5s(args.fast5_dir, args.signal_size, args.max_start_end_margin,
-                                     args.min_signal_length)
+    barcode_calls = get_porechop_barcode_calls(args.porechop_out)
 
     print('\t'.join(['Read_ID', 'Barcode_bin',
                      'Start_read_signal', 'Middle_read_signal', 'End_read_signal']))
-    
-    with open(args.porechop_out, 'rt') as porechop_output:
+
+    fast5_files = find_all_fast5s(args.fast5_dir)
+
+    print('', file=sys.stderr)
+    print('Loading signal from {} fast5 files'.format(len(fast5_files)), end='', file=sys.stderr)
+
+    i, short_count, bad_signal_count = 0, 0, 0
+
+    for fast5_file in fast5_files:
+        i += 1
+        if i % 100 == 0:
+            print('.', end='', file=sys.stderr, flush=True)
+
+        read_id, signal = get_read_id_and_signal(fast5_file)
+
+        if read_id is None or read_id not in barcode_calls:
+            continue
+        if len(signal) < args.min_signal_length:
+            short_count += 1
+            continue
+
+        barcode_bin = barcode_calls[read_id]
+        bad_signal = False
+
+        # The middle signal is simply the centre-most signal in the read.
+        middle_pos = len(signal) // 2
+        middle_1 = middle_pos - (args.signal_size // 2)
+        middle_2 = middle_pos + (args.signal_size // 2)
+        middle_signal = signal[middle_1:middle_2]
+
+        start_margin = args.signal_size * 2
+        while too_much_open_pore(signal[:start_margin]):
+            start_margin += args.signal_size
+            if start_margin > args.max_start_end_margin:
+                bad_signal = True
+                break
+
+        end_margin = args.signal_size * 2
+        while too_much_open_pore(signal[-end_margin:]):
+            end_margin += args.signal_size
+            if end_margin > args.max_start_end_margin:
+                bad_signal = True
+                break
+
+        if bad_signal:
+            bad_signal_count += 1
+            continue
+
+        start_signal = signal[:start_margin]
+        end_signal = signal[-end_margin:]
+
+        start_signal_str = ','.join(str(x) for x in start_signal)
+        middle_signal_str = ','.join(str(x) for x in middle_signal)
+        end_signal_str = ','.join(str(x) for x in end_signal)
+
+        print('\t'.join([read_id, str(barcode_bin),
+                         start_signal_str, middle_signal_str, end_signal_str]))
+
+    print(' done', file=sys.stderr)
+    print('skipped ' + str(short_count) + ' reads for being too short\n', file=sys.stderr)
+    print('skipped ' + str(bad_signal_count) + ' reads for bad signal stdev\n', file=sys.stderr)
+
+
+def get_porechop_barcode_calls(porechop_output_filename):
+    print('', file=sys.stderr)
+    print('Getting barcode calls from Porechop output... ', file=sys.stderr, end='', flush=True)
+    barcode_calls = {}
+    with open(porechop_output_filename, 'rt') as porechop_output:
         for line in porechop_output:
             m = re.search(r'^\w{8}-\w{4}-\w{4}-\w{4}-\w{12}', line)
             if m:
@@ -28,23 +95,11 @@ def training_data_from_porechop(args):
                         read_info.append(line)
                     else:  # empty line indicates the end of one read's info
                         break
-
-                if read_id not in signals:
-                    continue
-
                 barcode_bin = get_final_barcode_call(read_info)
-                barcode_bin, start_coord, end_coord = get_start_end_coords(barcode_bin, read_info)
-                if barcode_bin is None:
-                    continue
-
-                start_signal, middle_signal, end_signal = signals[read_id]
-
-                start_signal_str = ','.join(str(x) for x in start_signal)
-                middle_signal_str = ','.join(str(x) for x in middle_signal)
-                end_signal_str = ','.join(str(x) for x in end_signal)
-
-                print('\t'.join([read_id, str(barcode_bin),
-                                 start_signal_str, middle_signal_str, end_signal_str]))
+                if barcode_bin is not None:
+                    barcode_calls[read_id] = barcode_bin
+    print(' done', file=sys.stderr)
+    return barcode_calls
 
 
 def get_final_barcode_call(read_info):
@@ -55,41 +110,3 @@ def get_final_barcode_call(read_info):
         return str(int(barcode_bin[2:]))
     else:
         return None
-
-
-def get_start_barcode_coords(read_info, barcode_bin):
-    return get_coords(read_info, barcode_bin, 'start alignments:', 'end alignments:')
-
-
-def get_end_barcode_coords(read_info, barcode_bin):
-    return get_coords(read_info, barcode_bin, 'end alignments:', 'Barcodes:')
-
-
-def get_coords(read_info, barcode_bin, line_1, line_2):
-    i = read_info.index(line_1)
-    j = read_info.index(line_2)
-    alignment_info = read_info[i+1:j]
-    alignment_info = [x for x in alignment_info
-                      if x.startswith('Barcode ' + str(barcode_bin) + ' (')][0]
-    coords = alignment_info.split('read position: ')[-1]
-    return [int(x) for x in coords.split('-')]
-
-
-def get_end_margin_size(read_info):
-    end_seq_line = [x for x in read_info if x.startswith('end: ')][0]
-    end_seq = end_seq_line.split('...')[-1]
-    end_seq = re.sub('\033.*?m', '', end_seq)  # remove formatting
-    return len(end_seq)
-
-
-def get_start_end_coords(barcode_bin, read_info):
-    if barcode_bin:
-        try:
-            end_margin = get_end_margin_size(read_info)
-            start_coord = get_start_barcode_coords(read_info, barcode_bin)[1]
-            end_coord = end_margin - get_end_barcode_coords(read_info, barcode_bin)[0]
-            return barcode_bin, start_coord, end_coord
-        except (IndexError, ValueError):
-            return None, '', ''
-    else:
-        return barcode_bin, '', ''
